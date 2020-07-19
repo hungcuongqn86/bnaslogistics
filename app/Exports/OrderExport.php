8f86bbe6e6f3ada192e33d0514969ef837e712a0
cuongnh <hungcuongqn86@gmail.com>
2020-07-20 04:04:38 +0700

    excell

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -4,14 +4,43 @@ namespace App\Exports;
 
 use Modules\Common\Entities\Order;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Support\Collection;
 
-class OrderExport implements FromCollection
+class OrderExport implements FromCollection, WithHeadings
 {
+    public function headings(): array
+    {
+        return [
+            'Đơn hàng',
+            'Link sp',
+        ];
+    }
+
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        return Order::all();
+        $query = Order::with(['Cart'])->where('is_deleted', '=', 0);
+        $orderData = $query->get(['id'])->toArray();
+        $data = new Collection();
+        foreach ($orderData as $order) {
+            foreach ($order['cart'] as $key => $cart) {
+                if (!$key) {
+                    $data[] = array(
+                        'id' => $order['id'],
+                        'link' => $cart['pro_link']
+                    );
+                } else {
+                    $data[] = array(
+                        'id' => '',
+                        'link' => $cart['pro_link']
+                    );
+                }
+            }
+        }
+
+        return collect($data);
     }
 }
