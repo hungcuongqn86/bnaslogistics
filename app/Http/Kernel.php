d3a19f883ec6103701de6aa8e10c4e24da39a142
cuongnh <hungcuongqn86@gmail.com>
2020-09-15 02:07:45 +0700

    cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,10 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'addtk' => \App\Http\Middleware\AddToken::class,
+    ];
+
+    protected $middlewarePriority = [
+        \App\Http\Middleware\AddToken::class,
     ];
 }
