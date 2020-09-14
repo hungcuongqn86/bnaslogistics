5c6af2905d3f8999a6cc73cb00083e7525d95c0a
cuongnh <hungcuongqn86@gmail.com>
2020-09-15 02:35:18 +0700

    cors

--- routes/api/cart.php
+++ routes/api/cart.php
@@ -2,9 +2,11 @@
 Route::group(['middleware' => 'auth:api'], function () {
     Route::group(['prefix' => 'cart', 'namespace' => 'Modules\Cart\Http\Controllers'], function () {
         Route::get('/search', 'CartController@search');
-        Route::post('/create', 'CartController@create')->middleware('cors');
         Route::post('/update', 'CartController@update');
         Route::post('/delete', 'CartController@delete');
     });
 });
 
+Route::group(['prefix' => 'cart', 'namespace' => 'Modules\Cart\Http\Controllers'], function () {
+    Route::post('/create', 'CartController@create');
+});
