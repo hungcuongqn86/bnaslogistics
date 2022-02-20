000b4a0ebe6c380e95a32120a77cdefd40735832
cuongnh <hungcuongqn86@gmail.com>
2022-02-21 04:45:09 +0800

    cart

--- routes/api/cart.php
+++ routes/api/cart.php
@@ -2,8 +2,12 @@
 Route::group(['middleware' => 'auth:api'], function () {
     Route::group(['prefix' => 'cart', 'namespace' => 'Modules\Cart\Http\Controllers'], function () {
         Route::get('/search', 'CartController@search');
-        Route::post('/update', 'CartController@itemUpdate');
+        Route::post('/update', 'CartController@update');
         Route::post('/delete', 'CartController@delete');
+        Route::group(['prefix' => 'cart_item', 'namespace' => 'Modules\Cart\Http\Controllers'], function () {
+            Route::post('/update', 'CartController@itemUpdate');
+            Route::post('/delete', 'CartController@itemDelete');
+        });
     });
 });
 
