1701b4d9d15daca582f27f4dc037bad5b275bdcb
cuongnh <hungcuongqn86@gmail.com>
2022-05-24 20:34:31 +0800

    export

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -2,20 +2,19 @@
 
 namespace App\Exports;
 
-use Modules\Common\Entities\Order;
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
-
-use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Modules\Common\Entities\Order;
 
 class OrderExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents
 {
     public function registerEvents(): array
     {
         return [
-            AfterSheet::class    => function(AfterSheet $event) {
+            AfterSheet::class => function (AfterSheet $event) {
                 $cellRange = 'A1:W1';
                 $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
             },
@@ -28,14 +27,13 @@ class OrderExport implements FromCollection, WithHeadings, ShouldAutoSize, WithE
             'Đơn hàng',
             'Trạng thái',
             'Ngày tạo',
-            'Shop',
             'Tên khách hàng',
             'SĐT khách hàng',
             'Email khách hàng',
-            'Tiền hàng',
+            'Tổng tiền',
             'Thanh toán',
             'Còn thiếu',
-            'Thực hiện',
+            'Phụ trách',
             'Link sp',
         ];
     }
@@ -45,51 +43,65 @@ class OrderExport implements FromCollection, WithHeadings, ShouldAutoSize, WithE
      */
     public function collection()
     {
-        $query = Order::with(['User', 'Cart', 'Shop', 'Handle'])->where('is_deleted', '=', 0);
+        $query = Order::with(['User', 'OrderItems', 'Handle']);
         $query->orderBy('id', 'desc');
         $orderData = $query->get()->toArray();
         $arr_status = [
             '1' => 'Chờ báo giá',
             '2' => 'Chờ đặt cọc',
             '3' => 'Đang mua hàng',
+            '4' => 'Đã mua hàng',
             '5' => 'Thanh lý',
             '6' => 'Hủy',
         ];
         $data = new Collection();
         foreach ($orderData as $order) {
-            foreach ($order['cart'] as $key => $cart) {
-                if (!$key) {
-
-                    $data[] = array(
-                        'id' => $order['id'],
-                        'status' => !empty($arr_status[$order['status']]) ? $arr_status[$order['status']] : $order['status'],
-                        'created_at' => date('d-m-Y', strtotime($order['created_at'])),
-                        'shop' => $order['shop']['name'],
-                        'user_name' => $order['user']['name'],
-                        'user_email' => $order['user']['email'],
-                        'user_phone_number' => $order['user']['phone_number'],
-                        'tien_hang' => (int)$order['tong'],
-                        'thanh_toan' => (int)$order['thanh_toan'],
-                        'thieu' => (int)$order['tong'] - (int)$order['thanh_toan'],
-                        'handle_name' => !empty($order['handle']['name']) ? $order['handle']['name'] : '',
-                        'link' => $cart['pro_link']
-                    );
-                } else {
-                    $data[] = array(
-                        'id' => '',
-                        'status' => '',
-                        'created_at' => '',
-                        'shop' => '',
-                        'user_name' => '',
-                        'user_email' => '',
-                        'user_phone_number' => '',
-                        'tien_hang' => '',
-                        'thanh_toan' => '',
-                        'thieu' => '',
-                        'handle_name' => '',
-                        'link' => $cart['pro_link']
-                    );
+            if (!empty($order['order_items'])) {
+                foreach ($order['order_items'] as $key => $order_item) {
+                    if (!$key) {
+                        $data[] = array(
+                            'id' => $order['code'],
+                            'status' => !empty($arr_status[$order['status']]) ? $arr_status[$order['status']] : $order['status'],
+                            'created_at' => date('d-m-Y', strtotime($order['created_at'])),
+                            'user_name' => $order['user']['name'],
+                            'user_email' => $order['user']['email'],
+                            'user_phone_number' => $order['user']['phone_number'],
+                            'tien_hang' => (int)$order['tien_hang'],
+                            'thanh_toan' => (int)$order['dat_coc'],
+                            'thieu' => (int)$order['tien_hang'] - (int)$order['dat_coc'],
+                            'handle_name' => !empty($order['handle']['name']) ? $order['handle']['name'] : '',
+                            'link' => $order_item['pro_link']
+                        );
+                    } else {
+                        $data[] = array(
+                            'id' => '',
+                            'status' => '',
+                            'created_at' => '',
+                            'user_name' => '',
+                            'user_email' => '',
+                            'user_phone_number' => '',
+                            'tien_hang' => '',
+                            'thanh_toan' => '',
+                            'thieu' => '',
+                            'handle_name' => '',
+                            'link' => $order_item['pro_link']
+                        );
+                    }
                 }
+            } else {
+                $data[] = array(
+                    'id' => $order['code'],
+                    'status' => !empty($arr_status[$order['status']]) ? $arr_status[$order['status']] : $order['status'],
+                    'created_at' => date('d-m-Y', strtotime($order['created_at'])),
+                    'user_name' => $order['user']['name'],
+                    'user_email' => $order['user']['email'],
+                    'user_phone_number' => $order['user']['phone_number'],
+                    'tien_hang' => (int)$order['tien_hang'],
+                    'thanh_toan' => (int)$order['dat_coc'],
+                    'thieu' => (int)$order['tien_hang'] - (int)$order['dat_coc'],
+                    'handle_name' => !empty($order['handle']['name']) ? $order['handle']['name'] : '',
+                    'link' => ''
+                );
             }
         }
 
