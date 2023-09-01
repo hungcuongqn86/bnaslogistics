85ded83df731171ddd29a579947368ae6616fb04
hungcuongqn86 <hungcuongqn86@gmail.com>
2023-09-01 13:04:44 +0800

    expỏt

--- modules/Order/Http/Controllers/OrderController.php
+++ modules/Order/Http/Controllers/OrderController.php
@@ -967,7 +967,7 @@ class OrderController extends CommonController
         try {
             // Transaction
             $datcoc = $order['tien_hang'] + $order['phi_kiem_dem_tt'] + $order['phi_dat_hang_tt'];
-            $datcoc = ceil($datcoc * $vip['deposit'] / 100);
+            $datcoc = round($datcoc * $vip['deposit'] / 100);
             if ($datcoc != $input['dc_value']) {
                 return $this->sendError('Error', ['Lỗi dữ liệu, hãy thực hiện lại!']);
             }
